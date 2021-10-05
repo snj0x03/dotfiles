98dbbcf817337bc9f716dcd9fe1cc31072635600
Salamandeyz <Salamandeyz>
2021-10-05 10:11:22 +0630

    new file:   Screenshots/_000 	new file:   Screenshots/_001 	new file:   Screenshots/_002 	new file:   alacritty.yml 	new file:   fehbg 	new file:   i3-config 	new file:   nvim 	new file:   picom.conf 	new file:   polybar/color.ini 	new file:   polybar/config.ini 	new file:   polybar/launch.sh 	new file:   polybar/modules.ini 	new file:   ranger/rc.conf 	new file:   ranger/rifle.conf 	new file:   rofi/launcher/colors.rasi 	new file:   rofi/launcher/launcher.sh 	new file:   rofi/launcher/style.rasi 	new file:   rofi/powermenu/color.rasi 	new file:   rofi/powermenu/powermenu.sh 	new file:   rofi/powermenu/theme.rasi 	new file:   webkit2-greeter.conf 	new file:   zshrc

---
+++ rofi/powermenu/powermenu.sh
@@ -0,0 +1,46 @@
+theme="theme"
+dir="$HOME/.dotfiles/rofi/powermenu"
+
+uptime=$(uptime -p | sed -e 's/up //g')
+
+rofi_command="rofi -theme $dir/$theme"
+
+
+shutdown=""
+reboot=""
+lock=""
+suspend=""
+logout=""
+
+options="$shutdown\n$reboot\n$lock\n$suspend\n$logout"
+
+chosen="$(echo -e "$options" | $rofi_command -p "Uptime: $uptime" -dmenu -selected-row 2)"
+case $chosen in
+    $shutdown)
+		systemctl poweroff
+        ;;
+    $reboot)
+	    systemctl reboot
+        ;;
+    $lock)
+		if [[ -f /usr/bin/i3lock ]]; then
+			i3lock
+		elif [[ -f /usr/bin/betterlockscreen ]]; then
+			betterlockscreen -l
+		fi
+        ;;
+    $suspend)
+		mpc -q pause
+		amixer set Master mute
+		systemctl suspend
+        ;;
+    $logout)
+        if [[ "$DESKTOP_SESSION" == "Openbox" ]]; then
+            openbox --exit
+        elif [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
+            bspc quit
+        elif [[ "$DESKTOP_SESSION" == "i3" ]]; then
+            i3-msg exit
+        fi
+        ;;
+esac
---
+++ zshrc
@@ -0,0 +1,1 @@
+/home/soul/.zshrc
